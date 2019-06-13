f4bc85ed2436b6f62bbb727c62e0daf05a7b4e0b
Mason Thomas <mason@Masons-Mac-mini.local>
2019-06-13 15:31:56 -0500

    added new prompt

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 
 var data;
+var prompt;
+
+var prompt1blank = '___! they said ___ as they jumped into their ___ and flew off with their ___ ___.';
+var prompt2blank = 'Driving a car can be fun if you follow this ___ advice: When approaching an ___ on the right, always blow your ___, Before making an ___ turn, always stick your ___ out of the window. Every 2000 miles, have your ___ inspected and your ___ checked. When approaching a school, watch out for ___ ___. Above all, drive ___ The ___ you save may be your own!'
 
 var prompt1 = '$$Exclamation$$! they said $$Adverb$$ as they jumped into their $$Noun$$ and flew off with their $$Adjective$$ $$PluralNoun$$.';
+var prompt2 = 'Driving a car can be fun if you follow this $$Adjective$$ advice: When approaching an $$Noun$$ on the right, always blow your $$Noun$$, Before making an $$Adjective$$ turn, always stick your $$Noun$$ out of the window. Every 2000 miles, have your $$Noun$$ inspected and your $$Noun$$ checked. When approaching a school, watch out for $$Adjective$$ $$PluralNoun$$. Above all, drive $$Adverb$$ .The $$Noun$$ you save may be your own!'
 
 function setup() {
   noCanvas();
@@ -10,6 +15,20 @@ function setup() {
     callback: gotData,
     simpleSheet: true
   });
+
+  var random = Math.floor(Math.random() * 2);
+
+ if (random == 0) {
+    promptblank = prompt1blank;
+    prompt = prompt1;
+ }
+
+ if (random == 1) {
+    promptblank = prompt2blank;
+    prompt = prompt2;
+ }
+
+  document.getElementById('promptblank').innerHTML = promptblank;
 
   var button = createButton('generate madlib');
   button.mousePressed(generate);
@@ -23,7 +42,7 @@ function replacer(match, pos) {
 
 function generate() {
   //console.log('generate');
-  var madlib = prompt1.replace(/\$\$(.*?)\$\$/g, replacer);
+  var madlib = prompt.replace(/\$\$(.*?)\$\$/g, replacer);
   createP(madlib);
 }
 
